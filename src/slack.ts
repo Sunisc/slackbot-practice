05f51f746e3b55df6fb83973d5fa99be8707deab
Sunisc <surya.poddutoori@gmail.com>
2023-10-19 17:46:59 -0400

    practice with slack bots

--- src/slack.ts
+++ src/slack.ts
@@ -1,7 +1,47 @@
 import type { Handler } from '@netlify/functions';
 
-export const handler: Handler = async () => {
+import { parse } from 'querystring';
+import { slackApi } from './util/slack';
+
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+	switch (payload.command) {
+		case '/foodfight':
+			const joke = await fetch('https://icanhazdadjoke.com', {
+				method: 'GET',
+				headers: { accept: 'text/plain' }
+			});
+			const response = await slackApi('chat.postMessage', {
+				channel: payload.channel_id,
+				text: await joke.text(),
+			});
+
+			if (!response.ok) {
+				console.log(response)
+			}
+
+			break;
+
+		default:
+			return {
+				statusCode: 200,
+				body: `Command ${payload.command} is not recognized`
+			}
+	}
+
+	return {
+		statusCode: 200,
+		body: ''
+	}
+}
+
+export const handler: Handler = async (event) => {
 	// TODO validate the Slack request
+
+	const body = parse(event.body ?? '') as SlackPayload;
+
+	if (body.command) {
+		return handleSlashCommand(body as SlackSlashCommandPayload)
+	}
 
 	// TODO handle slash commands
 
